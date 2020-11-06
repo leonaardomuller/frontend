47ce719ed2fe89a1619505268e8cc962f995f50f
Leonardo Muller <leonaardo.muller@gmail.com>
2020-11-05 21:54:38 -0300

    commit Final

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from './components/Header'
 
 function App() {
 
-  const [projects, setProjects] = useState(['Desenvolvimento WEB', 'Aplicativo']);
+  const [projects, setProjects] = useState([]);
 
   //useState retorna um array com duas posições
   //
@@ -15,7 +15,7 @@ function App() {
 
   useEffect(() => {
     api.get('projects').then(response => {
-      console.log(response)
+      setProjects(response.data);
     })
   }, [])
 
@@ -24,10 +24,18 @@ function App() {
   //1. Qual função quero disparar
   //2. Quando eu quero disparar essa função
 
-  function handleAddProject() {
+  async function handleAddProject() {
+  const response = await api.post('projects', {
+    title: `Novo projeto${Date.now()}`,
+    owner: "Leo mestre do universo"
+  }) 
+  const project = response.data;
+
+  setProjects([...projects, project])
+
     //projects.push(`Novo projeto${Date.now()}`); //altera o valor de projects
 
-    setProjects([...projects, `Novo projeto${Date.now()}`]); //cria um novo project com o valor que eu definir
+    //setProjects([...projects, `Novo projeto${Date.now()}`]); //cria um novo project com o valor que eu definir
     //pega o todos os valores de project passa o valor que eu quero, e cria um novo.
   }
 
@@ -36,7 +44,7 @@ function App() {
       <Header title="Projects" />
 
       <ul>
-  {projects.map(project => <li key="project">{project}</li>)}
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
       </ul>
 
       <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
